8c265b7a86ca89ec93f34614bf23747f06eed306
Ugo-J <ezeorahjeffrey@gmail.com>
2024-03-30 00:42:09 +0100

    initial commit

---
+++ lockws_headers.hpp
@@ -0,0 +1,48 @@
+#ifndef IOSTREAM
+#define IOSTREAM
+
+#include <iostream>
+
+#endif
+
+#ifndef OPENSSL_BIO_H
+#define OPENSSL_BIO_H
+
+#include <openssl/bio.h>
+
+#endif
+
+#ifndef OPENSSL_SSL_H
+#define OPENSSL_SSL_H
+
+#include <openssl/ssl.h>
+
+#endif
+
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+#include <signal.h>
+
+#endif
+
+#ifndef CSTRING
+#define CSTRING
+
+#include <cstring>
+
+#endif
+
+#ifndef CHRONO
+#define CHRONO
+
+#include <chrono>
+
+#endif
+
+#ifndef FUNCTIONAL
+#define FUNCTIONAL
+
+#include <functional>
+
+#endif
