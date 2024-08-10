72099193d3f774572b677613e5c83ca0e3eca2e6
Ugo-J <ezeorahJeffrey@gmail.com>
2024-08-10 20:48:18 +0100

    made the define guards more specific to prevent clashing with other include header guards

--- lockws_headers.hpp
+++ lockws_headers.hpp
@@ -1,47 +1,47 @@
-#ifndef IOSTREAM
-#define IOSTREAM
+#ifndef LOCK_WS_IOSTREAM
+#define LOCK_WS_IOSTREAM
 
 #include <iostream>
 
 #endif
 
-#ifndef OPENSSL_BIO_H
-#define OPENSSL_BIO_H
+#ifndef LOCK_WS_OPENSSL_BIO_H
+#define LOCK_WS_OPENSSL_BIO_H
 
 #include <openssl/bio.h>
 
 #endif
 
-#ifndef OPENSSL_SSL_H
-#define OPENSSL_SSL_H
+#ifndef LOCK_WS_OPENSSL_SSL_H
+#define LOCK_WS_OPENSSL_SSL_H
 
 #include <openssl/ssl.h>
 
 #endif
 
-#ifndef SIGNAL_H
-#define SIGNAL_H
+#ifndef LOCK_WS_SIGNAL_H
+#define LOCK_WS_SIGNAL_H
 
 #include <signal.h>
 
 #endif
 
-#ifndef CSTRING
-#define CSTRING
+#ifndef LOCK_WS_CSTRING
+#define LOCK_WS_CSTRING
 
 #include <cstring>
 
 #endif
 
-#ifndef CHRONO
-#define CHRONO
+#ifndef LOCK_WS_CHRONO
+#define LOCK_WS_CHRONO
 
 #include <chrono>
 
 #endif
 
-#ifndef FUNCTIONAL
-#define FUNCTIONAL
+#ifndef LOCK_WS_FUNCTIONAL
+#define LOCK_WS_FUNCTIONAL
 
 #include <functional>
 
