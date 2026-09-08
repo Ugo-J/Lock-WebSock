d3a1b626661879b7fd02aea11045109bccff926b
Ugo-J <ezeorahJeffrey@gmail.com>
2026-09-08 23:27:04 +0100

    created the openssl poll mode client scaffolding for the crtp and non-crtp variants

---
+++ openssl/lockws_pm_headers.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <signal.h>
+#include <cstring>
+#include <chrono>
+#include <functional>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <atomic>
+#include <thread>
+#include <charconv>
