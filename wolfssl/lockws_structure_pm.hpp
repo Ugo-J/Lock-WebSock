a240c2601563106f4f4dd108d6171503668fca05
Ugo-J <ezeorahJeffrey@gmail.com>
2026-09-26 08:15:12 +0100

    implemented the clear data function in the wolfssl non-crtp poll mode client

--- wolfssl/lockws_structure_pm.hpp
+++ wolfssl/lockws_structure_pm.hpp
@@ -28,6 +28,10 @@ public:
     bool basic_read();
     bool set_ping_backlog(int backlog_num);
     bool clear(); // this function is used to clear the error flags of lock clients in open state, error flags of lock clients in closed state can only be cleared by calling the connect function
+
+    // function to clear any unread data from the read buffer this function simply sets the read last read to the read last write
+    void clear_data(){ read_last_read.store(read_last_write.load(std::memory_order_acquire), std::memory_order_release); }
+
     inline static int default_pong_receive(char*, int, int); // default function for receiving pong frames
     inline static int default_receive(char*, int, int); // default receive function called by basic read
     void set_receive_function(lock_function fn);
